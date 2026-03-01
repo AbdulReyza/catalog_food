09c6472bc1c1718ee868e4f283ebf103e47682aa
AbdulReyza <abdulreyza6@gmail.com>
2026-03-02 06:27:53 +0700

    mengganti run ke buildApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:catalog_food/core/constants/app_constant.dart';
 import 'package:catalog_food/core/routes/app_route.dart';
+import 'package:catalog_food/injection.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(buildApp());
 }
 
 class MyApp extends StatelessWidget {
